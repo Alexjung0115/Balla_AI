f618c37ccb727f36acd21b267c212777ac82958e
Alexjung0115 <64525881+Alexjung0115@users.noreply.github.com>
2025-06-05 21:18:23 +0900

    에러 수정

--- main.py
+++ main.py
@@ -38,15 +38,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-print(1)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
-print(2)
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
-print(3)
+
 model1 = YOLO("best_weight_model/newfaceeye.pt").to(device)
-print(4)
+
 model2 = YOLO("best_weight_model/bestyolo8n_withoutacne.pt").to(device)
-print(5)
 
 class WrinkleRegressionEfficientNet(nn.Module):
     def __init__(self):
@@ -131,19 +130,12 @@ class RegressionEfficientNet(nn.Module):
         elif self.label =='Pigmentation':
             x = self.act(x) * 7.0  # 등급 범위에 맞게 조절
         return x
-print(6)
 wrinkle_model = RegressionEfficientNet('Wrinkle').to(device)
-print(7)
 wrinkle_model.load_state_dict(torch.load("best_weight_model/best_wrinkle_model.pth", map_location=device))
-print(8)
 wrinkle_model.eval()
-print(9)
 Pore_model = RegressionEfficientNet('Pore').to(device)
-print(10)
 Pore_model.load_state_dict(torch.load("best_weight_model/Pore_best_model_2nd.pth", map_location=device))
-print(11)
 Pore_model.eval()
-print(12)
 Pig_model = RegressionEfficientNet('Pigmentation').to(device)
 Pig_model.load_state_dict(torch.load("best_weight_model/Pigmentation_best_model_2nd.pth", map_location=device))
 Pig_model.eval()
@@ -153,7 +145,6 @@ Sagging_model.eval()
 Dry_model = RegressionEfficientNet('Dry').to(device)
 Dry_model.load_state_dict(torch.load("best_weight_model/Dry_best_model_2nd.pth", map_location=device))
 Dry_model.eval()
-print(13)
 # ✅ 입력 전처리 (EfficientNetB0은 224x224 입력 기준)
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -161,7 +152,6 @@ transform = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406],
                          [0.229, 0.224, 0.225])
 ])
-print(14)
 part_models = {
     'forehead': [("Wrinkle", wrinkle_model), ("Pigmentation", Pig_model)],
     'glabella': [("Wrinkle", wrinkle_model)],
@@ -172,7 +162,6 @@ part_models = {
     'lips': [("Dry", Dry_model)],
     'chin': [("Sagging", Sagging_model)]
 }
-print(15)
 def predict_score(crop, model):
     print("predict")
     input_tensor = transform(crop).unsqueeze(0).to(device)
@@ -244,19 +233,19 @@ async def analyze_image(file: UploadFile = File(...)):
    
     img_path = input_path
     results = model1(img_path)
-    print(21)
+
     boxes = results[0].boxes
     cls = boxes.cls.cpu().numpy()
     xyxy = boxes.xyxy.cpu().numpy()
     conf = boxes.conf.cpu().numpy()
-    print(22)
+
     face_indices = [i for i, (c, cf) in enumerate(zip(cls, conf)) if c == 1 and cf >= 0.7]
     eye_indices = [i for i, c in enumerate(cls) if c == 0]
-    print(23)
+
     img = cv2.imread(img_path)
     result_msg = ""
     show_image = False
-    print(24)
+
     if face_indices:
         face_boxes = [xyxy[i] for i in face_indices]
         if len(face_boxes) == 1:
@@ -274,53 +263,54 @@ async def analyze_image(file: UploadFile = File(...)):
                     pil_img = Image.fromarray(img_rgb)
 
                     results2 = model2(pil_img)
-                    #위에꺼 안되면 다시 삭제할 것것
-                    if boxes2 is not None and any(cls.item() != 0 for cls in boxes2.cls):
-                        results2[0].save(filename=output_path)
                     
-                    # if results2[0].boxes is not None and len(results2[0].boxes) > 0:
-                    #     results2[0].save(filename=output_path)
+                    if results2[0].boxes is not None and len(results2[0].boxes) > 0:
+                         results2[0].save(filename=output_path)
                         
                         boxes2 = results2[0].boxes
 
                         cls2 = boxes2.cls.cpu().numpy()
                         xyxy2 = boxes2.xyxy.cpu().numpy()
-                        
-                        index_mapping = {
-                            'forehead': [i for i, c in enumerate(cls2) if c == 1],
-                            'glabella': [i for i, c in enumerate(cls2) if c == 2],
-                            'l_periocular': [i for i, c in enumerate(cls2) if c == 3],
-                            'r_periocular': [i for i, c in enumerate(cls2) if c == 4],
-                            'l_cheek': [i for i, c in enumerate(cls2) if c == 5],
-                            'r_cheek': [i for i, c in enumerate(cls2) if c == 6],
-                            'lips': [i for i, c in enumerate(cls2) if c == 7],
-                            'chin': [i for i, c in enumerate(cls2) if c == 8],
-                        }
-                        results_by_part = {}
-
-
-                        # === 각 부위별로 crop 및 예측 수행 ===
-                        for part, indices in index_mapping.items():
-                            if not indices:
-                                continue  # 해당 부위가 탐지되지 않으면 skip
-
-                            # 첫 번째 탐지된 부위 기준으로 crop
-                            x1, y1, x2, y2 = map(int, xyxy2[indices[0]])
-                            crop = pil_img.crop((x1, y1, x2, y2))
-
-                            part_results = {}
-                            for label, model in part_models.get(part, []):
-                                score = predict_score(crop, model)
-                                part_results[label] = round(score, 2)
-
-                            if part_results:
-                                results_by_part[part] = part_results
-                                
-                            final_result = convert_results(results_by_part)
-                        
-                        result_msg = final_result
-                        show_image = True
-                        status_code = status.HTTP_200_OK
+                        if any(cls.item() != 0 for cls in boxes2.cls:
+                            index_mapping = {
+                                'forehead': [i for i, c in enumerate(cls2) if c == 1],
+                                'glabella': [i for i, c in enumerate(cls2) if c == 2],
+                                'l_periocular': [i for i, c in enumerate(cls2) if c == 3],
+                                'r_periocular': [i for i, c in enumerate(cls2) if c == 4],
+                                'l_cheek': [i for i, c in enumerate(cls2) if c == 5],
+                                'r_cheek': [i for i, c in enumerate(cls2) if c == 6],
+                                'lips': [i for i, c in enumerate(cls2) if c == 7],
+                                'chin': [i for i, c in enumerate(cls2) if c == 8],
+                            }
+                            results_by_part = {}
+    
+    
+                            # === 각 부위별로 crop 및 예측 수행 ===
+                            for part, indices in index_mapping.items():
+                                if not indices:
+                                    continue  # 해당 부위가 탐지되지 않으면 skip
+    
+                                # 첫 번째 탐지된 부위 기준으로 crop
+                                x1, y1, x2, y2 = map(int, xyxy2[indices[0]])
+                                crop = pil_img.crop((x1, y1, x2, y2))
+    
+                                part_results = {}
+                                for label, model in part_models.get(part, []):
+                                    score = predict_score(crop, model)
+                                    part_results[label] = round(score, 2)
+    
+                                if part_results:
+                                    results_by_part[part] = part_results
+                                    
+                                final_result = convert_results(results_by_part)
+                            
+                            result_msg = final_result
+                            show_image = True
+                            status_code = status.HTTP_200_OK
+                        else:
+                            result_msg = "피부를 식별하지 못했습니다."
+                            status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+                            final_result = result_msg
                     else:
                         result_msg = "유의미한 결과가 없습니다. 이미지를 다시 선택해주세요."
                         status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
